98502a3c539ec8e460844e81061caa4b5e1f789f
jospaquim <joshephpm1@gmail.com>
2024-07-08 11:49:02 -0500

    add files

---
+++ SistemaInventario2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaInventario2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
